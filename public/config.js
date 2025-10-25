df20c235552a2acb7a0f2159b5f3c1d64a2cb234
Mario Reder <mario.reder@pm.me>
2025-10-25 21:59:11 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "DGO Dex",
   "VITE_SEO_SITE_DESCRIPTION": "Your Decentalized Exchange",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/dgo-5131/",
+  "VITE_SEO_SITE_URL": "https://dgo.ad",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
